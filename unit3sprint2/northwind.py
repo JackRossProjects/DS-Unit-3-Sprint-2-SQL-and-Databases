9262f0a88081c594704a5bd294cdc09e736735e1
Jack Ross <43938405+JackRossProjects@users.noreply.github.com>
2020-04-17 10:32:09 -0600

    Now follows PEP 8

--- unit3sprint2/northwind.py
+++ unit3sprint2/northwind.py
@@ -4,7 +4,8 @@ import sqlite3
 import os
 
 # Make connection and cursor for DB
-DB_FILEPATH = os.path.join(os.path.dirname(__file__), '/home/jack/Desktop/northwind_small.sqlite3')
+DB_FILEPATH = os.path.join(os.path.dirname(
+    __file__), '/home/jack/Desktop/northwind_small.sqlite3')
 conn = sqlite3.connect(DB_FILEPATH)
 curs = conn.cursor()
 
@@ -21,9 +22,11 @@ result = curs.execute(query).fetchall()
 print('P2Q1 Top ten most expensive products: ', result, '\n')
 
 # P2Q2: What is the average age of an employee at time of hiring?
-# Use SQL julianday function: https://www.w3resource.com/sqlite/sqlite-julianday.php
+# Use SQL julianday function:
+# https://www.w3resource.com/sqlite/sqlite-julianday.php
 query = """
-SELECT AVG(julianday(HireDate) - julianday(BirthDate)) / 365 AS "Average hire age"
+SELECT AVG(julianday(HireDate) - julianday(BirthDate)) /
+365 AS "Average hire age"
 FROM Employee
 """
 result = curs.execute(query).fetchall()
@@ -31,17 +34,18 @@ print('P2Q2 Average age of employee at hiring: ', result, '\n')
 
 # P2Q3: How does the average age of employee at hire vary by city?
 query = """
-SELECT City, AVG(julianday(HireDate) - julianday(BirthDate)) / 365 AS "Average hire age"
+SELECT City, AVG(julianday(HireDate) - julianday(BirthDate)) /
+365 AS "Average hire age"
 FROM Employee
 GROUP BY City
 """
 result = curs.execute(query).fetchall()
-print('P2Q3 How does average age of employee at hire vary by city: ', result, '\n')
+print('P2Q3 How avg age of employee at hire vary by city: ', result, '\n')
 
 
 print("--- Sprint Part 3 Questions ---\n")
 
-# P3Q1: What are the ten most expensive items (per unit price) in the database and their suppliers?
+# P3Q1: What are the ten most expensive items in database & their suppliers?
 query = """
 SELECT CompanyName, ProductName, UnitPrice AS "Price"
 FROM Product
@@ -51,7 +55,7 @@ ORDER BY UnitPrice DESC
 LIMIT 10
 """
 result = curs.execute(query).fetchall()
-print('P3Q1 Top ten most expensive products AND their suppliers: ', result, '\n')
+print('P3Q1 Top 10 most expensive products AND their supplier: ', result, '\n')
 
 # P3Q2: What is the largest category (by number of unique products in it)?
 query = """
@@ -66,7 +70,7 @@ LIMIT 1
 result = curs.execute(query).fetchall()
 print('P3Q2 Largest category by unique product: ', result, '\n')
 
-## P3Q3: Who's the employee with the most territories?
+# P3Q3: Who's the employee with the most territories?
 query = """
 SELECT LastName, FirstName, COUNT(EmployeeId) AS "TerritoryId"
 FROM Employee
